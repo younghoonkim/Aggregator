cef6ae27b34aed6501f62d14a4fe0501c67665f7
Jake <Jake@172.30.18.119>
2016-06-26 11:24:17 +0900

    wd

---
+++ Test/routes/index.js
@@ -0,0 +1,32 @@
+
+/*
+ * GET home page.
+ */
+
+module.exports = function(app)
+{
+	app.get('/',function(req,res){
+		var Client = require('node-rest-client').Client;
+		var client = new Client();
+		
+		client.get("http://localhost:8080/server/json", function(data,response){
+			
+			console.log(data);
+			res.render('index.html',data);
+		});
+		
+		
+	});
+	
+	app.get('/about',function(req,res){
+		res.render('about.html');
+	});
+};
+
+/*exports.index = function(req, res){
+	res.render('index', { title: 'Express' });
+  
+};
+exports.hello = function(req, res){
+	res.render('hello', { title: 'Hello Node'});
+};*/
